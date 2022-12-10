024627ff0a70a9762d1b9082c571d057841c5ca7
NAERNON <naernon@icloud.com>
2022-12-10 18:45:30 +0100

    PackedByteArray extensions improvements

--- Sources/Godot/Extensions/Builtin Structs/PackedByteArray+Extensions.swift
+++ Sources/Godot/Extensions/Builtin Structs/PackedByteArray+Extensions.swift
@@ -1,9 +1,22 @@
 import Foundation
 
 extension PackedByteArray {
+    public init(_ value: PackedByteArray) {
+        self.init(packedByteArray: value)
+    }
+    
     public func withUnsafeBytesArray(_ body: (UnsafeMutablePointer<UInt8>) -> Void) {
         self.withUnsafeNativePointer { nativeTypePtr in
             body(Self.interface.packed_byte_array_operator_index(nativeTypePtr, 0)!)
+        }
+    }
+    
+    public subscript(index: Int) -> UInt8 {
+        get {
+            UInt8(self._getValue(at: Int64(index)))
+        }
+        set(newValue) {
+            self._setValue(Int(newValue), at: Int64(index))
         }
     }
 }
