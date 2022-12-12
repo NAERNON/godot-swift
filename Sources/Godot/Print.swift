d7e6dc363cd5eef4423d26166c08776054c91bec
NAERNON <naernon@icloud.com>
2022-12-12 20:46:50 +0100

    Small print fix

--- Sources/Godot/Print.swift
+++ Sources/Godot/Print.swift
@@ -1,35 +1,15 @@
 import Foundation
 
-private func printGodot(message: Swift.String,
-                        function: Swift.String,
-                        file: Swift.String,
-                        line: Int,
-                        isError: Bool) {
-    if isError {
-        GodotInterface.native.print_error(message, function, file, Int32(line))
-    } else {
-        GodotInterface.native.print_warning(message, function, file, Int32(line))
-    }
-}
-
 public func printGodotError(_ message: Swift.String,
                             function: Swift.String = #function,
                             file: Swift.String = #file,
                             line: Int = #line) {
-    printGodot(message: message,
-          function: function,
-          file: file,
-          line: line,
-          isError: true)
+    GodotInterface.native.print_error(message, function, file, Int32(line))
 }
 
 public func printGodotWarning(_ message: Swift.String,
                               function: Swift.String = #function,
                               file: Swift.String = #file,
                               line: Int = #line) {
-    printGodot(message: message,
-          function: function,
-          file: file,
-          line: line,
-          isError: false)
+    GodotInterface.native.print_warning(message, function, file, Int32(line))
 }
