00801c8f40c982168b139c1be915205160091752
NAERNON <naernon@icloud.com>
2022-12-22 15:40:55 +0100

    Array protocol improvements

--- Sources/Godot/Extensions/Builtin Structs/Array+Extensions.swift
+++ Sources/Godot/Extensions/Builtin Structs/Array+Extensions.swift
@@ -1,8 +1,18 @@
 import Foundation
 
 extension Array {
+    // MARK: Init
+    
     public init(_ value: Array) {
-        self = value.duplicate()
+        self = value._duplicate()
+    }
+    
+    public init(repeating repeatedVariant: Variant, count: Int) {
+        var new = Array()
+        for _ in 0..<count {
+            new._append(repeatedVariant)
+        }
+        self = new
     }
     
     public subscript(index: Int) -> Variant {
@@ -13,6 +23,26 @@ extension Array {
             self._setValue(newValue, at: Int64(index))
         }
     }
+    
+    public mutating func replaceSubrange<C>(_ subrange: Range<Int>, with newElements: C)
+    where C : Collection, Variant == C.Element {
+        var rangeIndex = subrange.lowerBound
+        for (collectionIndex, variant) in newElements.enumerated() {
+            if collectionIndex + subrange.lowerBound < subrange.upperBound {
+                self[rangeIndex] = variant
+            } else {
+                self._insert(position: rangeIndex, value: variant)
+            }
+            rangeIndex += 1
+        }
+        
+        let removeIndex = subrange.lowerBound + newElements.count
+        while rangeIndex < subrange.upperBound {
+            self._removeAt(removeIndex)
+            rangeIndex += 1
+        }
+
+    }
 }
 
 extension Array: Sequence {}
@@ -21,11 +51,11 @@ extension Array: Collection {
     public var startIndex: Int {
         0
     }
-    
+
     public var endIndex: Int {
-        count
+        self._size()
     }
-    
+
     public func index(after i: Int) -> Int {
         i+1
     }
@@ -39,4 +69,16 @@ extension Array: BidirectionalCollection {
 
 extension Array: RandomAccessCollection {}
 
+extension Array: RangeReplaceableCollection {}
+
 extension Array: MutableCollection {}
+
+extension Array: ExpressibleByArrayLiteral {
+    public init(arrayLiteral elements: Variant...) {
+        var new = Array()
+        for element in elements {
+            new._append(element)
+        }
+        self = new
+    }
+}
