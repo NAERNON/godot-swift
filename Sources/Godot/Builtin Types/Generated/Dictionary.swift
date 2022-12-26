ffc09e63cb4d6dcb248f6d6f6e6c5e3818d9a513
NAERNON <naernon@icloud.com>
2022-12-26 14:15:44 +0100

    Dictionary now conform to the Sequence protocol

--- Sources/Godot/Builtin Types/Generated/Dictionary.swift
+++ Sources/Godot/Builtin Types/Generated/Dictionary.swift
@@ -18,6 +18,23 @@ extension Dictionary {
     public static func == (lhs: Variant, rhs: Dictionary) -> Bool {
         Self._operatorEqual(rhs, lhs)
     }
+    
+    public subscript(key: Variant) -> Variant {
+        get {
+            self._getValue(forKey: key)
+        }
+        set(newValue) {
+            self._set(value: newValue, forKey: key)
+        }
+    }
+    
+    public func keys() -> Array {
+        self._keys()
+    }
+    
+    public func values() -> Array {
+        self._values()
+    }
 }
 
 // MARK: - Extensions
@@ -25,6 +42,46 @@ extension Dictionary {
 extension Dictionary: Equatable {
     public static func == (lhs: Dictionary, rhs: Dictionary) -> Bool {
         Self._operatorEqual(lhs, rhs)
+    }
+}
+
+extension Dictionary: Sequence {
+    public struct Iterator: IteratorProtocol {
+        private let dictionary: Dictionary
+        private let keys: Array
+        private var index = 0
+        
+        fileprivate init(dictionary: Dictionary) {
+            self.dictionary = dictionary
+            self.keys = dictionary._keys()
+        }
+        
+        public mutating func next() -> (Variant, Variant)? {
+            guard index < keys.count else {
+                return nil
+            }
+            
+            let key = keys[index]
+            index += 1
+            return (key, dictionary[key])
+        }
+    }
+    
+    /// Returns an iterator over the dictionary’s key-value pairs.
+    ///
+    /// Due to Godot limitations, making the iterator is in O(*n*).
+    public func makeIterator() -> Iterator {
+        Iterator(dictionary: self)
+    }
+}
+
+extension Dictionary: ExpressibleByDictionaryLiteral {
+    public init(dictionaryLiteral elements: (Variant, Variant)...) {
+        var newDictionary = Dictionary()
+        for (key, value) in elements {
+            newDictionary[key] = value
+        }
+        self = newDictionary
     }
 }
 
