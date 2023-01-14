de7ba966a74361c8ea8e97c480294ad33f816a11
NAERNON <naernon@icloud.com>
2023-01-14 16:04:13 +0100

    SwiftString conform to TypedVariantTransformable

--- Sources/Godot/Extensions/SwiftString+Extensions.swift
+++ Sources/Godot/Extensions/SwiftString+Extensions.swift
@@ -15,3 +15,15 @@ extension Swift.String {
         self.init(godotString: Godot.String(stringName: godotStringName))
     }
 }
+
+extension Swift.String: TypedVariantTransformable {
+    public static var variantStorageType: Variant.ValueType { String.variantStorageType }
+    
+    public var variant: Variant {
+        String(swiftString: self).variant
+    }
+    
+    public init(variant: Variant) {
+        self = Swift.String(godotString: variant.stringValue)
+    }
+}
