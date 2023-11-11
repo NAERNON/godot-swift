269945cfa33ce25d0f5a68246453f2f40597d02d
NAERNON <naernon@icloud.com>
2023-11-11 11:26:58 +0100

    Text hint added

--- Sources/Godot/Godot Extension/Class Registrar/EditorHint.swift
+++ Sources/Godot/Godot Extension/Class Registrar/EditorHint.swift
@@ -357,6 +357,22 @@ public struct EditorHint {
             string: GodotString(stringName: type._$className)
         )
     }
+    
+    // MARK: Text
+    
+    /// Hints that a string property is text with line breaks.
+    /// Editing it will show a text input field where line breaks can be typed.
+    public static let multiline: EditorHint =
+        .init(hint: .multilineText, string: GodotString())
+    
+    /// Hints that a string property should have a placeholder
+    /// text visible on its input field, whenever the property is empty.
+    public static func placeholder(_ text: String) -> EditorHint {
+        .init(
+            hint: .placeholderText,
+            string: GodotString(swiftString: text)
+        )
+    }
 }
 
 // MARK: - Macro
